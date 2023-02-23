d188b46e88006815c2a6e5a5004eb24c632c4fc6
uaesmartlink <team.uaesmartlink@gmail.com>
2023-02-23 19:20:50 +0400

    fix customerController sms

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -118,14 +118,14 @@ class OrderController extends Controller
         if (!$request->hasValidSignature() || $order->hash != $hash) {
             abort(404);
         }
-        echo "<h1>Hello</h1>";
+        // echo "<h1>Hello</h1>";
 
-        // $order->load(['account', 'customer', 'services', 'taxes', 'user:id,name', 'payments']);
-        // return Inertia::render('Orders/Show', [
-        //     'modal' => false,
-        //     'order' => $order->toArray(),
-        //     'hash' => $hash,
-        // ]);
+        $order->load(['account', 'customer', 'services', 'taxes', 'user:id,name', 'payments']);
+        return Inertia::render('Orders/Show', [
+            'modal' => false,
+            'order' => $order->toArray(),
+            'hash' => $hash,
+        ]);
     }
 
     public function status(Request $request, Order $order)
