96b7731d5ba095e6368af0cd9c1e4ad69ffc3957
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-01 17:07:18 +0400

    add  code first

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -138,10 +138,8 @@ class OrderController extends Controller
         if ($order->hash != $hash) {
             abort(404);
         }
-        $order->load(['account', 'customer', 'services', 'taxes', 'user:id,name', 'payments']);
-        $order->status = "Preparing";
+        $order->status = 'Preparing';
         $order->save();
-        $order = Order::withoutGlobalScopes()->where('id', $order)->first();
         $order->load(['account', 'customer', 'services', 'taxes', 'user:id,name', 'payments']);
 
         // return $order;
