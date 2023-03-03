45be5f3ac8554343b7537aff2b8dc0fbd0c2ac9b
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-03 13:03:30 +0400

    test canve

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,4 +1,4 @@
-<html>
+{{-- <html>
     <head>
         <title>
             Order
@@ -502,4 +502,18 @@
         </div>
     </body>
 </html>
-
+ --}}
+ <html>
+	<head>
+	</head>
+	<body>
+		<div style="position: relative; width: 100%; height: 0; padding-top: 50.0000%;
+	 padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+	 border-radius: 8px; will-change: transform;">
+	  <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
+		src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFcIAziZjQ&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+	  </iframe>
+	</div>
+	<a href="" target="_blank" rel="noopener">Modern Email Signature Template</a> by smart link
+	</body>
+</html>
