515fcbcd9976ce6ebe4df15414df18baa56c1b4b
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-07 12:52:00 +0400

    new 'mission report'

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -23,7 +23,7 @@
             overflow: hidden;
             border: 1px solid #ccc;
             background-color: #f1f1f1;
-            height: 5%;
+            height: 7%;
             }
 
             /* Style the buttons inside the tab */
