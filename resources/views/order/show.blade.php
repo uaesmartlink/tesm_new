22897261eca0d6516002260b2c4712da07e65542
uaesmartlink <team.uaesmartlink@gmail.com>
2023-03-07 12:48:57 +0400

    new 'mission report'

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -29,7 +29,7 @@
             .tab button {
             background-color: inherit;
             position: relative;
-            left: 30%;
+            left: 50%;
             float: left;
             border: none;
             outline: none;
